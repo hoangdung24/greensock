e08e998168b3f3141aa5ab1debe599b8a076b18a
unknown <conquynho.hd@gmail.com>
2023-02-21 12:14:21 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  root: resolve(__dirname),
-  plugins: [tsconfigPaths()],
-  publicDir: resolve(__dirname, "public"),
-  css: {},
-  build: {
-    minify: "esbuild",
-  },
-  esbuild: {},
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+
+  return {
+    root: resolve(__dirname),
+    plugins: [tsconfigPaths()],
+    publicDir: resolve(__dirname, "public"),
+    css: {},
+    build: {
+      minify: "esbuild",
+    },
+    base: mode === "production" ? "/greensock/" : "/",
+    esbuild: {},
+  };
 });
