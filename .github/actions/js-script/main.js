0aef798d493c0f7de5233bba6013c4097c55c4fe
unknown <conquynho.hd@gmail.com>
2023-03-22 21:18:07 +0700

    test js action

---
+++ .github/actions/js-script/main.js
@@ -0,0 +1,10 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
+function run() {
+  // console.log("HELLO WORLD FROM GITHUB ACTION");
+  core.notice("HELLO WORLD FROM GITHUB ACTION");
+}
+
+run();
